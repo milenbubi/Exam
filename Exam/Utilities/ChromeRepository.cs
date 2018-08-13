833d807a5345a0d4360d357b8e94bcf55775cd42
milenbubi <milenbubi@abv.bg>
2018-08-13 16:47:02 +0300

    little fixes

--- Exam/Utilities/ChromeRepository.cs
+++ Exam/Utilities/ChromeRepository.cs
@@ -2,23 +2,55 @@
 using OpenQA.Selenium.Chrome;
 using System.IO;
 using System.Reflection;
+using System;
 
 namespace Exam.Utilities
 {
     public static class ChromeRepository
     {
-        public static IWebDriver Driver { get; private set; }
+        private static IWebDriver driver;
+        private static string chromePath;
+
+        static ChromeRepository()
+        {
+            chromePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (driver == null)
+                {
+                    throw new NullReferenceException("ChromeDriver is not initialized! Please use appropriate methods to create one!");
+                }
+                else
+                {
+                    return driver;
+                }
+            }
+            private set
+            {
+                driver = value;
+            }
+        }
 
         public static IWebDriver InitDriver()
         {
-            Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver = new ChromeDriver(chromePath);
             return Driver;
         }
 
         public static IWebDriver InitDriver(ChromeOptions options)
         {
-            Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
+            driver = new ChromeDriver(chromePath, options);
             return Driver;
+        }
+
+        public static void ShutDownDriver()
+        {
+            driver.Quit();
+            driver = null;
         }
     }
 }
